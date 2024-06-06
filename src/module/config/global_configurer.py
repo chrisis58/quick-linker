e098abfb4c15d2495772c5667d86d5fc471fce40
chrisis <chrisis58@foxmail.com>
2024-06-06 16:24:09 +0800

    style: 优化全局配置类
    - 添加了获取实例方法的注释文档
    - 添加了获取实力防范的类型提示

--- src/module/config/global_configurer.py
+++ src/module/config/global_configurer.py
@@ -1,10 +1,12 @@
 from src.module.decorator import Singleton
-from src.module.bean.quick_linker_config import QuickLinkerConfig
+from typing import Type, TypeVar
+
+T = TypeVar('T')
 
 @Singleton
 class GlobalConfigurer:
     """
-    singleton configurer
+    singleton global configurer
     """
 
     def __init__(self):
@@ -13,7 +15,14 @@ class GlobalConfigurer:
     def set_global_config(self, **kwargs):
         self._global_config = kwargs
 
-    def get_config_instance(self, cls, **kwargs):
+    def get_config_instance(self, cls: Type[T], **kwargs) -> T:
+        """
+        获取配置数据类实例，缺省的项以全局为准
+
+        :param cls: 要新建的配置数据类
+        :param kwargs: 传入的配置项
+        :return: 配置数据实例
+        """
         return cls(**{**self._global_config, **kwargs})
 
 
