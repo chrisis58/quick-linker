4fa2b60d1cf344e4d435fefb92d2240ab8accdc0
chrisis <chrisis58@foxmail.com>
2024-07-07 00:19:33 +0800

    feat: Add error handling for missing arguments in Singleton decorator

--- quick_linker/decorator/singleton.py
+++ quick_linker/decorator/singleton.py
@@ -2,6 +2,11 @@ def Singleton(cls):
     _instance = {}
     def _singleton(*args, **kwargs):
         if cls not in _instance:
-            _instance[cls] = cls(*args, **kwargs)
+            try:
+                _instance[cls] = cls(*args, **kwargs)
+            except TypeError as te:
+                print(f"TypeError: {te}")
+                print(f"arguments required: {cls.__init__.__code__.co_varnames}")
+                print(f"arguments provided: {args}")
         return _instance[cls]
     return _singleton
